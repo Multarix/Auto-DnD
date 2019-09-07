ab134978cebd9cebe3ce841221f809ea3241a647
Multarix <darien2105@gmail.com>
2019-09-07 16:12:25 +1000

    npc jobs

--- modules/npc/jobs/butcher.js
+++ modules/npc/jobs/butcher.js
@@ -8,12 +8,12 @@ module.export = () => {
 
 	for(i=0; 27>i; i++){
 		const number = Math.floor(Math.random() * 100 + 1);
-		if(number <= 30){ str = str++; continue; }; // 30% of points
-		if(number <= 40){ dex = dex++; continue; }; // 10% of points
-		if(number <= 60){ cont = cont++; continue; }; // 20% of points
-		if(number <= 80){ int = int++; continue; }; // 20% of points
-		if(number <= 90){ wis = wis++; continue; }; // 10% of points
-		if(number >= 91){ char = char++; continue; }; // 10% of points
+		if(number <= 30){ str++; continue; };		// 30% of points
+		if(number <= 40){ dex++; continue; };		// 10% of points
+		if(number <= 60){ cont++; continue; };	// 20% of points
+		if(number <= 80){ int++; continue; }; 	// 20% of points
+		if(number <= 90){ wis++; continue; }; 	// 10% of points
+		if(number >= 91){ char++; continue; }; 	// 10% of points
 	};
 	const stats = {"str":str, "dex":dex, "cont":cont, "int":int, "wis":wis, "char":char};
 	return stats;
