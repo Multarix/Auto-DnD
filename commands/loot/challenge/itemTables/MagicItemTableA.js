df39cb82e71cb75be2ed7d8738bd3c9764d3f8a0
Multarix <darien2105@gmail.com>
2019-09-11 18:16:16 +1000

    Format Changes

--- commands/loot/challenge/itemTables/MagicItemTableA.js
+++ commands/loot/challenge/itemTables/MagicItemTableA.js
@@ -1,10 +1,10 @@
 module.exports = (roll) => {
-	if(roll <= 50) return "Potion of Healing";
-	if(roll <= 60) return "Spell Scroll (Cantrip)";
-	if(roll <= 70) return "Potion of Climbing";
-	if(roll <= 90) return "Spell Scroll (1st level)";
-	if(roll <= 94) return "Spell Scroll (2nd level)";
-	if(roll <= 98) return "Potion of Greater Healing";
-	if(roll === 99) return "Bag of Holding";
-	if(roll === 100) return "Driftglobe";
+	if(roll <= 50) return "1 **Potion of Healing**\n";
+	if(roll <= 60) return "1 **Spell Scroll (Cantrip)**\n";
+	if(roll <= 70) return "1 **Potion of Climbing**\n";
+	if(roll <= 90) return "1 **Spell Scroll (1st level)**\n";
+	if(roll <= 94) return "1 **Spell Scroll (2nd level)**\n";
+	if(roll <= 98) return "1 **Potion of Greater Healing**\n";
+	if(roll === 99) return "1 **Bag of Holding**\n";
+	if(roll === 100) return "1 **Driftglobe**\n";
 };
--- commands/loot/challenge/itemTables/MagicItemTableE.js
+++ commands/loot/challenge/itemTables/MagicItemTableE.js
@@ -1,9 +1,9 @@
 module.exports = (roll) => {
-	if(roll <= 30) return "Spell Scroll (8th level)";
-	if(roll <= 55) return "Potion of Storm Giant Strength";
-	if(roll <= 70) return "Potion of Supreme Healing";
-	if(roll <= 85) return "Spell Scroll (9th level)";
-	if(roll <= 93) return "Univeral Solvent";
-	if(roll <= 98) return "Arrow of Slaying";
-	if(roll <= 100) return "Sovereign Glue";
+	if(roll <= 30) return "1 **Spell Scroll (8th level)**\n";
+	if(roll <= 55) return "1 **Potion of Storm Giant Strength**\n";
+	if(roll <= 70) return "1 **Potion of Supreme Healing**\n";
+	if(roll <= 85) return "1 **Spell Scroll (9th level)**\n";
+	if(roll <= 93) return "1 **Univeral Solvent**\n";
+	if(roll <= 98) return "1 **Arrow of Slaying**\n";
+	if(roll <= 100) return "1 **Sovereign Glue**\n";
 };
