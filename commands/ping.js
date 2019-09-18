5d3df4c9ebd4591767700151205f1cfcbb10243c
Multarix <darien2105@gmail.com>
2019-09-18 20:32:37 +1000

    Ping Command

---
+++ commands/ping.js
@@ -0,0 +1,20 @@
+exports.run = (client, message, args, level) => {
+
+	message.channel.send('Pinging...').then(sent => {
+		sent.edit(`Pong! Took ${sent.createdTimestamp - message.createdTimestamp}ms\nHeartbeat ping is: ${Math.round(client.ping)}ms`);
+	});
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+	permLevel: 10,
+};
+
+exports.help = {
+	name: "ping",
+	category: "Misc",
+	description: "Gets the bot's ping",
+	usage: "..",
+};
