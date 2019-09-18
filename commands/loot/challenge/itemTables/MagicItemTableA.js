059de5852e41b294c8543db058abeab97cd678d7
Multarix <darien2105@gmail.com>
2019-09-18 20:32:49 +1000

    Updated format

--- commands/loot/challenge/itemTables/MagicItemTableA.js
+++ commands/loot/challenge/itemTables/MagicItemTableA.js
@@ -1,10 +1,10 @@
 module.exports = (roll) => {
-	if(roll <= 50) return "1 **Potion of Healing**\n";
-	if(roll <= 60) return "1 **Spell Scroll (Cantrip)**\n";
-	if(roll <= 70) return "1 **Potion of Climbing**\n";
-	if(roll <= 90) return "1 **Spell Scroll (1st level)**\n";
-	if(roll <= 94) return "1 **Spell Scroll (2nd level)**\n";
-	if(roll <= 98) return "1 **Potion of Greater Healing**\n";
-	if(roll === 99) return "1 **Bag of Holding**\n";
-	if(roll === 100) return "1 **Driftglobe**\n";
+	if(roll <= 50) return { name:"**Potion of Healing**", link:"https://www.dndbeyond.com/magic-items/potion-of-healing" };
+	if(roll <= 60) return { name:"**Spell Scroll (Cantrip)**", link:"https://www.dndbeyond.com/magic-items/spell-scroll" };
+	if(roll <= 70) return { name:"**Potion of Climbing**", link:"https://www.dndbeyond.com/magic-items/potion-of-climbing" };
+	if(roll <= 90) return { name:"**Spell Scroll (1st level)**", link:"https://www.dndbeyond.com/magic-items/spell-scroll" };
+	if(roll <= 94) return { name:"**Spell Scroll (2nd level)**", link:"https://www.dndbeyond.com/magic-items/spell-scroll" };
+	if(roll <= 98) return { name:"**Potion of Greater Healing**", link:"https://www.dndbeyond.com/magic-items/potion-of-healing" };
+	if(roll === 99) return { name:"**Bag of Holding**", link:"https://www.dndbeyond.com/magic-items/bag-of-holding" };
+	if(roll === 100) return { name:"**Driftglobe**", link:"https://aversten.obsidianportal.com/wikis/driftglobe" };
 };
--- commands/loot/challenge/itemTables/MagicItemTableE.js
+++ commands/loot/challenge/itemTables/MagicItemTableE.js
@@ -1,9 +1,9 @@
 module.exports = (roll) => {
-	if(roll <= 30) return "1 **Spell Scroll (8th level)**\n";
-	if(roll <= 55) return "1 **Potion of Storm Giant Strength**\n";
-	if(roll <= 70) return "1 **Potion of Supreme Healing**\n";
-	if(roll <= 85) return "1 **Spell Scroll (9th level)**\n";
-	if(roll <= 93) return "1 **Universal Solvent**\n";
-	if(roll <= 98) return "1 **Arrow of Slaying**\n";
-	if(roll <= 100) return "1 **Sovereign Glue**\n";
+	if(roll <= 30) return { name:"**Spell Scroll (8th level)**", link:"https://www.dndbeyond.com/magic-items/spell-scroll" };
+	if(roll <= 55) return { name:"**Potion of Storm Giant Strength**", link:"https://www.dndbeyond.com/magic-items/potion-of-giant-strength" };
+	if(roll <= 70) return { name:"**Potion of Supreme Healing**", link:"https://www.dndbeyond.com/magic-items/potion-of-healing" };
+	if(roll <= 85) return { name:"**Spell Scroll (9th level)**", link:"https://www.dndbeyond.com/magic-items/spell-scroll" };
+	if(roll <= 93) return { name:"**Universal Solvent**", link:"https://www.dndbeyond.com/magic-items/universal-solvent" };
+	if(roll <= 98) return { name:"**Arrow of Slaying**", link:"https://www.dndbeyond.com/magic-items/arrow-of-slaying" };
+	if(roll <= 100) return { name:"**Sovereign Glue**", link:"https://www.dndbeyond.com/magic-items/sovereign-glue" };
 };
