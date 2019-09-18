9e7e08e49af5ffaa56c77ddb23b55c57db18467a
Multarix <darien2105@gmail.com>
2019-09-18 20:23:00 +1000

    Donation Command (Meme)

---
+++ commands/donate.js
@@ -0,0 +1,21 @@
+exports.run = (client, message, args) => {
+	return message.channel.send(`Don't be an idiot and throw your money away donating to discord bots.
+Donating to Discord bots is the dumbest idea I've seen UNLESS the bot is extremely popular;
+This bot is only in \`${client.guilds.size}\` server(s).
+
+Don't get me wrong, I'd love your money, but I'm not a "Please donate to me" bot at this point.`);
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+	permLevel: 0,
+};
+
+exports.help = {
+	name: "donate",
+	category: "Misc",
+	description: "Donate me Plox",
+	usage: "..",
+};
