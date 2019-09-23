7d92426bd7987e9a2ea18f560bbbf99b5e99803b
Multarix <darien2105@gmail.com>
2019-09-23 10:53:02 +1000

    Sets activity again on reconnecting

--- events/resume.js
+++ events/resume.js
@@ -1,5 +1,8 @@
+const colors = require("colors");
 module.exports = client => {
+
 	client.log("Re-established a connection to the websocket.", "Notify");
+	client.user.setActivity("with Dice", { type: 'PLAYING' }).then(client.log("Now playing " + colors.white("[") + "with Dice" + colors.white("]"), "Activity"));
 };
 
 module.exports.help = {
