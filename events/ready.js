45afd85a55efe4d913b021013cdfcdb31fddd44b
Multarix <darien2105@gmail.com>
2019-09-23 10:52:27 +1000

    Minor Change

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ module.exports = async client => {
 	client.log(`Logged in as ${client.user.tag}
     > Accessing a total of '${client.guilds.size}' server(s) With a total of '${client.users.size}' users\nReady called at: ${time}`, "Ready");
 
-	if(client.user.id === "304230184494563329") client.user.setActivity("with Dice", { type: 'PLAYING' }).then(client.log("Now playing " + colors.white("[") + "with Dice" + colors.white("]"), "Activity"));
+	client.user.setActivity("with Dice", { type: 'PLAYING' }).then(client.log("Now playing " + colors.white("[") + "with Dice" + colors.white("]"), "Activity"));
 	client.channels.filter(c => c.type === "text" && c.memberPermissions(c.guild.me).has("VIEW_CHANNEL")).forEach(c => c.fetchMessages({ limit: 5 }));
 };
 
