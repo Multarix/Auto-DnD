1ad7684f94c222c3bfed7b81418019deea3cad71
Multarix <darien2105@gmail.com>
2019-09-26 16:42:30 +1000

    Stop ignoring ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	apps : [{
+		name: "dnd",
+		script: "./dnd.js",
+		watch: ["events", "modules", "./dnd.js"],
+		ignore_watch: ["node_modules"],
+		cron_restart : "0 6 * * 7",
+		max_restarts: 10,
+		env: {
+			NODE_ENV: 'development',
+		},
+		env_production : {
+			NODE_ENV: 'production',
+		},
+	}],
+
+	deploy : {
+		production : {
+			user: 'node',
+			host: '212.83.163.1',
+			ref: 'origin/master',
+			repo: 'git@github.com:repo.git',
+			path: '/var/www/production',
+			'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+		},
+	},
+};
+
+// git branch -D <branchName>
