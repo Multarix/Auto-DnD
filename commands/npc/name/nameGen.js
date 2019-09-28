d82d7c7bc8231e659adfd86d9386445919913440
Multarix <darien2105@gmail.com>
2019-09-28 11:43:34 +1000

    NPC Name Generation I think this is complete now?

--- commands/npc/name/nameGen.js
+++ commands/npc/name/nameGen.js
@@ -2,8 +2,12 @@ module.exports = (character) => {
 	const gender = (Math.random() > 0.5) ? "male" : "female";
 	character.gender = gender.toProperCase();
 
-	const first = require(`./${gender}/firstname.json`);
-	const last = require("./lastname.json");
+	const reggie = /\w+$/g;	// eslint-disable-line no-useless-escape
+	const str = character.race.toLowerCase().replace("-", "");
+	const race = reggie.exec(str)[0];
+
+	const first = require(`./${gender}/${race}.json`);
+	const last = require(`./lastNames/${race}.json`);
 
 	const r1 = Math.floor(Math.random() * first.length);
 	const r2 = Math.floor(Math.random() * last.length);
