104e9c14aeee7c885563ab4edcb4cfa68081cf84
Multarix <darien2105@gmail.com>
2019-09-30 14:13:57 +1000

    Minor tweaks

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
 	apps : [{
 		name: "dnd",
 		script: "./dnd.js",
-		watch: ["events", "modules", "./dnd.js"],
+		watch: ["commands", "events", "modules", "./dnd.js"],
 		ignore_watch: ["node_modules"],
 		cron_restart : "0 6 * * 7",
-		exp_backoff_restart_delay: 500,
+		restart_delay: 5000,
 		max_restarts: 10,
 		env: {
 			NODE_ENV: 'development',
