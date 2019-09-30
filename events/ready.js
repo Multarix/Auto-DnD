d21a43e6cd29bc7cea5c8abf5f84339b7b0aa9b8
Multarix <darien2105@gmail.com>
2019-09-30 23:02:14 +1000

    activity changes

--- events/ready.js
+++ events/ready.js
@@ -7,8 +7,12 @@ module.exports = async client => {
 	client.log(`Logged in as ${client.user.tag}
     > Accessing a total of '${client.guilds.size}' server(s) With a total of '${client.users.size}' users\nReady called at: ${time}`, "Ready");
 
-	client.user.setActivity("with Dice", { type: 'PLAYING' }).then(client.log("Now playing " + colors.white("[") + "with Dice" + colors.white("]"), "Activity"));
 	client.channels.filter(c => c.type === "text" && c.memberPermissions(c.guild.me).has("VIEW_CHANNEL")).forEach(c => c.fetchMessages({ limit: 5 }));
+
+	let game = "with Guildbot";
+	if(client.user.id === "304230184494563329") game = "with Dice";
+	if(client.user.id === "628082697553575941") game = "D&D Debugger";
+	client.user.setActivity(game, { type: 'PLAYING' }).then(client.log("Now playing " + colors.white("[") + game + colors.white("]"), "Activity"));
 };
 
 module.exports.help = {
