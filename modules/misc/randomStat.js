b36f5b78e0baa7dafd63bca51ea69b96645df13c
Multarix <darien2105@gmail.com>
2019-10-01 23:40:50 +1000

    Format changes

--- modules/misc/randomStat.js
+++ modules/misc/randomStat.js
@@ -7,27 +7,27 @@ module.exports = (character, obj) => {
 	}
 	total -= 27;
 
-	while (total < 0 && character.stats[obj.mainStat] !== 15){
+	while(total < 0 && character.stats[obj.mainStat] !== 15){
 		character.stats[obj.mainStat] += 1;
 		total += 1;
 		if(character.stats[obj.mainStat] >= 14) total += 1;
 	}
 
 	if(obj.secondStat){
-		while (total < 0 && character.stats[obj.secondStat] !== 14){
+		while(total < 0 && character.stats[obj.secondStat] !== 14){
 			character.stats[obj.secondStat] += 1;
 			total += 1;
 			if(character.stats[obj.secondStat] >= 14) total += 1;
 		}
 	}
 
-	while (total < 0 && character.stats[obj.saveThrow] !== 14){
+	while(total < 0 && character.stats[obj.saveThrow] !== 14){
 		character.stats[obj.saveThrow] += 1;
 		total += 1;
 		if(character.stats[obj.saveThrow] >= 14) total += 1;
 	}
 
-	while (total < 0){
+	while(total < 0){
 		const num = randomNumber(obj.miscStats.length);
 		if(character.stats[obj.miscStats[num]] < 13){	character.stats[obj.miscStats[num]] += 1;	total += 1;	}
 	}
