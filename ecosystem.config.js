2d91ad01c57d69cedfe2195b23ccd4b1d40daae7
Multarix <darien2105@gmail.com>
2019-10-08 12:52:57 +1000

    Minor changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
 			user: 'node',
 			host: '212.83.163.1',
 			ref: 'origin/master',
-			repo: 'git@github.com:repo.git',
+			repo: 'https://github.com/Multarix/Auto-DnD.git',
 			path: '/var/www/production',
 			'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
 		},
