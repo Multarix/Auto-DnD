0efd8896a90a25b458563e88bcd48fec3a9c97fc
Multarix <darien2105@gmail.com>
2019-10-20 18:29:39 +1000

    Updated command for dnd-npc v1.1

--- commands/npc.js
+++ commands/npc.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const character = require("dnd-npc");
+const dnd = require("dnd-npc");
 const roles = require("./npc/roles.json");
 const races = require("./npc/races.json");
 exports.run = async (client, message, args) => {
@@ -10,7 +10,7 @@ exports.run = async (client, message, args) => {
 	const npcRole = (roles.includes(args[1].toLowerCase())) ? args[1].toLowerCase() : undefined;
 	if(!npcRace || !npcRole) return await message.channel.send("Usage: [npc](<..race> <..role/class>)", { code: "markdown" });
 
-	const npc = new character({ raceType: npcRace, roleType: npcRole }).generate();
+	const npc = new dnd.npc({ raceType: npcRace, roleType: npcRole }).generate();
 
 	let stats = "";
 	for(const [key, value] of Object.entries(npc.class.stats)){
@@ -26,31 +26,29 @@ exports.run = async (client, message, args) => {
 		npc.inventory.tools.forEach(t => {
 			tools.push(`[${t.name}](${t.link})`);
 		});
-		tools = tools.join(", ");
+		tools = tools.join("\n");
 	}
-
-	const charInfo = `**Race/ Subrace:**
-		[${npc.race.name}](${npc.race.link})
-		**Class:**
-		[${npc.class.name}](https://www.dndbeyond.com/classes/${npc.class.link})
-		**Gender:**
-		${npc.character.gender}
-		**Speed:**
-		${npc.race.speed}
-		\u200b
-		**Weapon:**
-		${weapon}
-		**Armor:**
-		${armor}
-		**Tools:**
-		${tools}`;
+	tools += "\n\u200b";
 
 	const embed = new Discord.MessageEmbed()
-		.setAuthor(npc.character.name)
+		.setTitle(`**Name:** \u200b \u200b ${npc.character.name}`)
 		.setFooter(client.user.username, client.user.displayAvatarURL())
 		.setTimestamp()
-		.addField("Misc Information", charInfo.removeIndents(), true)
-		.addField("**Stats**", `${stats}`, true);
+		.addField("Race/ Subrace", `[${npc.race.name}](${npc.race.link})`, true)
+		.addField("Class", `[${npc.class.name}](https://www.dndbeyond.com/classes/${npc.class.link})`, true)
+		.addField("Alignment", npc.character.alignment, true)
+		.addField("Size", npc.race.size, true)
+		.addField("Speed", npc.race.speed, true)
+		.addField("Gender", npc.character.gender, true)
+		.addField("Tools", tools, true)
+		.addField("Weapon", weapon, true)
+		.addField("Armor", armor, true)
+		.addField("Strength", npc.class.stats.strength, true)
+		.addField("Dexterity", npc.class.stats.dexterity, true)
+		.addField("Constitution", npc.class.stats.constitution, true)
+		.addField("Intelligence", npc.class.stats.intelligence, true)
+		.addField("Wisdom", npc.class.stats.wisdom, true)
+		.addField("Charisma", npc.class.stats.charisma, true);
 
 	return message.channel.send({ embed }).catch(e => errFunc(e));
 };
